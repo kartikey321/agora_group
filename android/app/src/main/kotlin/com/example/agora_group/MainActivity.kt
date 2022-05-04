9f87ea6d6149fc9c92732f3d0c224087df42f1a6
unknown <kartikeymahawar1234@gmail.com>
2022-05-04 17:13:08 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/agora_group/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.agora_group
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/app_id.dart
@@ -0,0 +1,1 @@
+var appID = 'f7792319ecf343f785405cd6bee6b078';
